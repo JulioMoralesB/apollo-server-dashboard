fd41d807b9b8eef26e0c1f18e81135b13e30e495
Julio Morales <101440577+JulioMoralesB@users.noreply.github.com>
2026-03-15 20:01:17 -0600

    feat: create base dashbord layout - closes #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import ServiceCard from "./components/ServiceCard"
+
+const services = [
+  { name: "Minecraft Server", status: "online" },
+  { name: "Discord Bot", status : "offline" },
+  { name: "Free Games Notifier", status: "online" },
+]
+
 function App() {
   return (
-    <div>
+    <div className="dashboard">
       <h1>Apollo Server Dashboard</h1>
+      <div className="services-grid">
+        {services.map((service, index) => (
+          <ServiceCard key={index} name={service.name} status={service.status} />
+        ))}
+      </div>
     </div>
   )
 }
